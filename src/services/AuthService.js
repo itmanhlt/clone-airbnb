b749c02383caeca24669d7d18c8fa2e2acbed423
manh le <it.manhlt@gmail.com>
2023-05-11 01:28:14 +0700

    fix error sign up

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,4 +2,5 @@ import { https } from "./config";
 
 export const authService = () => ({
   sigIn: (account) => https.post("/api/auth/signin", account),
+  sigUp: (account) => https.post("/api/auth/signup", account),
 });
