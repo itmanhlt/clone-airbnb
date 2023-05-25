19a7dba1e0f7a74ea45c89acb28feb0be95b06f1
manh le <it.manhlt@gmail.com>
2023-05-25 19:36:03 +0700

    update

--- src/Layouts/Layout.js
+++ src/Layouts/Layout.js
@@ -6,12 +6,12 @@ import Footer from "../Components/Footer/Footer";
 export default function Layout({ Components }) {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
-    <>
+    <div className="flex flex-col h-full min-h-screen">
       <HeaderOtherPage />
       <div className={`flex-grow ${isMobile ? "pt-[80px]" : "pt-[80px]"}`}>
         <Components />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
--- src/Pages/User/RoomByCityPage/RoomBycityPage.js
+++ src/Pages/User/RoomByCityPage/RoomBycityPage.js
@@ -19,18 +19,26 @@ export default function RoomBycityPage() {
       });
   }, [params]);
 
+  let renderContent = () => {
+    if (room.length !== 0) {
+      document.getElementById("map")?.classList.remove("md:hidden");
+      return `Danh sách phòng ở khu vực ${city}`;
+    } else {
+      document.getElementById("map")?.classList.add("md:hidden");
+      return `Không còn phòng trống ở khu vực ${city}`;
+    }
+  };
+
   return (
     <div className="px-5 md:px-8">
-      <div className="text-center pb-6 font-medium">
-        Danh sách phòng ở khu vực {city}
-      </div>
+      <div className="text-center pb-6 font-medium">{renderContent()}</div>
       <div className="flex">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-1 lg:grid-cols-2 auto-rows-max content-center md:w-1/2 md:mr-8">
           {room.map((item, index) => {
             return <RoomItem room={item} key={index} />;
           })}
         </div>
-        <div className="w-1/2 hidden md:block">
+        <div id="map" className="w-1/2 hidden md:block">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7632681.71239111!2d104.05611139048933!3d15.956063999357486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31157a4d736a1e5f%3A0xb03bb0c9e2fe62be!2zVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1684950639297!5m2!1svi!2s"
             width="100%"
