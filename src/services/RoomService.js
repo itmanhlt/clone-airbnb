7b4adb0a7e8d4cad9d1a0c47c11ef99078d1a0fb
manh le <it.manhlt@gmail.com>
2023-06-09 18:51:24 +0700

    merge detail page

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -4,4 +4,5 @@ export const roomService = {
   getRoomList: () => https.get("/api/phong-thue"),
   getRoomOfLocation: (id) =>
     https.get(`/api/phong-thue/lay-phong-theo-vi-tri?maViTri=${id}`),
+  getRoomDetail: (id) => https.get(`/api/phong-thue/${id}`),
 };
--- src/services/locationService.js
+++ src/services/locationService.js
@@ -2,4 +2,5 @@ import { https } from "./config";
 
 export const locationService = {
   getLocate: () => https.get("/api/vi-tri"),
+  getLocationDetail: (id) => https.get(`/api/vi-tri/${id}`),
 };
